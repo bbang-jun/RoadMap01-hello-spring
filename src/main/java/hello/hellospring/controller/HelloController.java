b3953ffd691493989b8d2fb3535f62fd6d491dd5
bbangjun <lucid9dawn@gmail.com>
2023-01-30 20:07:01 +0900

    feat(HelloController): Add helloApi 메서드 생성(api 방식)

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,40 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    // API 방식
+    // 템플릿 방식과의 차이점은 view도 없이 return할 때의 문자 그대로 들어감.
+    @GetMapping("hello-string")
+    @ResponseBody // http의 body부(응답 body부)에 "hello " + name 데이터를 직접 넣어준다는 의미
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    // API 방식
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }

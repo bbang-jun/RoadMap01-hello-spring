eab9e17a8754afdeab4e73677791923eb775caa7
bbangjun <lucid9dawn@gmail.com>
2023-02-13 14:52:19 +0900

    feat(JpaMemberRepository): save 메서드를 통해 em의 persist 추가

--- src/main/java/hello/hellospring/repository/JpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -2,13 +2,23 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 public class JpaMemberRepository implements MemberRepository{
+
+    // jpa를 쓰려면 EntityManager를 주입받아야 함(EntityManager에 여러가지 정보들을 자동으로 만들어줌)
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
     @Override
     public Member save(Member member) {
-        return null;
+        em.persist(member);
+        return member;
     }
 
     @Override
